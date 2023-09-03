f53b23c31e494c66d4befc835449597a3c3c65d9
Yigit Kayali <kayaliyigit00@gmail.com>
2023-09-03 21:41:24 +0300

    redirection fixed

--- app/page.jsx
+++ app/page.jsx
@@ -12,13 +12,7 @@ const IndexPage = () => {
   return null; // You can return null or some loading content here
 };
 
-export async function getServerSideProps({ res }) {
-    // Redirect to /dashboard on the server-side
-    res.setHeader('Location', '/dashboard');
-    res.statusCode = 302; // 302 Found (Temporary Redirect)
-    res.end();
-  
-  }
+
   
 
 export default IndexPage;
